b488d0760df7b5e7e4aeca45056b694355f9032c
Matteo Ciucani <matteo.ciucani@stud.unifi.it>
2023-02-22 12:03:59 +0100

    rifinitura classi per i test

--- test/Test_User.cpp
+++ test/Test_User.cpp
@@ -1,32 +1,7 @@
 //
 // Created by Matteo on 14/02/2023.
 //
-#include "gtest/gtest.h"
-#include "../User.h"
-#include "ObserverMock.h"
 
-class Test_User: public ::testing::Test{
-protected:
-    void SetUp() override {
-        user = new User("Matteo", "MatteCiuc");
-    }
-    void TearDown() override {
-        delete user;
-    }
-    User* user;
-};
-
-TEST_F(Test_User, TestGetUsername) {
-    EXPECT_EQ("MatteCiuc", user->getUsername());
-}
-
-TEST_F(Test_User, TestAddList) {
-    Listonic* list = new Listonic("Mangiare");
-    Listonic* list2 = new Listonic("Latticini");
-    user->addList(list);
-    user->addList(list2);
-    EXPECT_EQ(2, user->getNumLists());
-}
 
 
 
--- test/test_listonic_Fixture.cpp
+++ test/test_listonic_Fixture.cpp
@@ -1,18 +1,76 @@
 //
 // Created by Matteo on 15/02/2023.
 //
+
 #include "gtest/gtest.h"
 #include "../Listonic.h"
+#include "ObserverMock.h"
 
-/*class test_Listonic: public ::testing::Test  {
+
+class test_Listonic: public ::testing::Test  {
 protected:
     void SetUp() override {
         list.addItem("Latte", 2, "Mangiare");
         list.addItem("Uova", 12, "Mangiare");
         list.addItem("Pane", 1, "Mangiare");
     }
-
+    void TearDown() override {
+        list = Listonic("Test List");
+    }
     Listonic list = Listonic("Test List");
-};*/
+};
+
+
+
+TEST_F(test_Listonic, AddItem) {
+    list.addItem("Mela", 3, "Frutta");
+    ASSERT_EQ(list.getItems().size(), 4);
+    Category cat_Fruit = Category("Frutta");
+    std::string name_product = "Mela";
+    Item item_mela = Item(name_product, 3, &cat_Fruit);
+    ASSERT_EQ(*(list.getItems()[3]), item_mela);
+    ASSERT_EQ(*(list.getCategories()["Frutta"]), cat_Fruit);
+}
+
+
+TEST_F(test_Listonic, RemoveItem) {
+    list.removeItem(1);
+    ASSERT_EQ(list.getItems().size(), 2);
+    Category cat_pointer = Category("Mangiare");
+    std::string name_product = "Pane";
+    Item pane = Item(name_product, 1, &cat_pointer);
+    ASSERT_EQ(*list.getItems()[1], pane);
+    ASSERT_EQ(*(list.getCategories()["Mangiare"]), cat_pointer);
+}
+
+TEST_F(test_Listonic, SetQuantity) {
+    list.setQuantity(0, 3);
+    ASSERT_EQ(list.getItems().size(), 3);
+    Category cat_pointer = Category("Mangiare");
+    std::string name_product = "Latte";
+    Item latte = Item(name_product, 3, &cat_pointer);
+    ASSERT_EQ(*list.getItems()[0], latte);
+    ASSERT_EQ(*(list.getCategories()["Mangiare"]), cat_pointer);
+}
+
+
+TEST_F(test_Listonic, AddObserver) {
+    ObserverMock* observer = new ObserverMock();
+    list.addObserver(observer);
+    list.addItem("Mela", 3, "Frutta");
+    EXPECT_EQ(observer->getList(), &list);
+    delete observer;
+}
+
+TEST_F(test_Listonic, RemoveObserver) {
+    ObserverMock* observer = new ObserverMock();
+    list.addObserver(observer);
+    list.removeItem(1);
+    list.removeObserver(observer);
+    list.addItem("Mela", 3, "Frutta");
+    EXPECT_EQ(observer->getList(), &list);
+    delete observer;
+}
+
 
 
